8a8dfab6183f70225283a0c851f29ef4ad5f95e2
riccardofio <riccardofio97@gmail.com>
2023-06-25 16:20:06 +0200

    Refactor mutation operators list

--- webview-ui/src/app/mutation-operators/mutation-operators.component.ts
+++ webview-ui/src/app/mutation-operators/mutation-operators.component.ts
@@ -96,49 +96,50 @@ class Operator {
 
     switch (name) {
       case "ACM": { this.longName = "Argument Change of overloaded Method call"; break; }
+      case "AMR" : { this.longName = "Array Member Replacement"; break; }
       case "AOR": { this.longName = "Assignment Operator Replacement"; break; }
       case "AVR": { this.longName = "Address Value Replacement"; break; }
       case "BCRD": { this.longName = "Break and Continue Replacement and Deletion"; break; }
-      case "BLR": { this.longName = "Boolean Literal Replacement"; break; }
       case "BOR": { this.longName = "Binary Operator Replacement"; break; }
+      case "BVR": { this.longName = "Boolean Value Replacement"; break; }
+      case "CAO": { this.longName = "Compound Assignment Optimization"; break; }
       case "CBD": { this.longName = "Catch Block Deletion"; break; }
-      case "CCD": { this.longName = "Contract Constructor Deletion"; break; }
+      case "CER": { this.longName = "Casting Expression Replacement"; break; }
       case "CSC": { this.longName = "Conditional Statement Change"; break; }
-      case "DLR": { this.longName = "Data Location Replacement"; break; }
+      case "DLO": { this.longName = "Data Location Optimization"; break; }
       case "DOD": { this.longName = "Delete Operator Deletion"; break; }
-      case "ECS": { this.longName = "Explicit Conversion to Smaller type"; break; }
       case "EED": { this.longName = "Event Emission Deletion"; break; }
-      case "EHC": { this.longName = "Exception Handling Change"; break; }
+      case "EHD": { this.longName = "Exception Handling Statement Deletion"; break; }
       case "ER": { this.longName = "Enum Replacement"; break; }
-      case "ETR": { this.longName = "Ether Transfer function Replacement"; break; }
-      case "FVR": { this.longName = "Function Visibility Replacement"; break; }
+      case "FVO": { this.longName = "Function Visibility Optimization"; break; }
       case "GVR": { this.longName = "Global Variable Replacement"; break; }
       case "HLR": { this.longName = "Hexadecimal Literal Replacement"; break; }
-      case "ILR": { this.longName = "Integer Literal Replacement"; break; }
       case "ICM": { this.longName = "Increments Mirror"; break; }
+      case "ISD": { this.longName = "Initialization Statement Deletion"; break; }
       case "LSC": { this.longName = "Loop Statement Change"; break; }
       case "PKD": { this.longName = "Payable Keyword Deletion"; break; }
+      case "PLR": { this.longName = "Precision Loss Replacement"; break; }
       case "MCR": { this.longName = "Mathematical and Cryptographic function Replacement"; break; }
-      case "MOC": { this.longName = "Modifier Order Change"; break; }
       case "MOD": { this.longName = "Modifier Deletion"; break; }
       case "MOI": { this.longName = "Modifier Insertion"; break; }
-      case "MOR": { this.longName = "Modifier Replacement"; break; }
+      case "NVR": { this.longName = "Number Value Replacement"; break; }
       case "OLFD": { this.longName = "Overloaded Function Deletion"; break; }
       case "OMD": { this.longName = "Overridden Modifier Deletion"; break; }
       case "ORFD": { this.longName = "Overridden Function Deletion"; break; }
+      case "RRI": { this.longName = "Revoke Role Insertion"; break; }
       case "RSD": { this.longName = "Return Statement Deletion"; break; }
+      case "RSI": { this.longName = "Require Statement Insertion"; break; }
       case "RVS": { this.longName = "Return Values Swap"; break; }
       case "SCEC": { this.longName = "Switch Call Expression Casting"; break; }
-      case "SFI": { this.longName = "Selfdestruct Insertion"; break; }
-      case "SFD": { this.longName = "Selfdestruct Deletion"; break; }
-      case "SFR": { this.longName = "SafeMath Function Replacement"; break; }
-      case "SKD": { this.longName = "Super Keyword Deletion"; break; }
-      case "SKI": { this.longName = "Super Keyword Insertion"; break; }
-      case "SLR": { this.longName = "String Literal Replacement"; break; }
+      case "SFID": { this.longName = "Selfdestruct Function Insertion Deletion"; break; }
+      case "SKID": { this.longName = "Super Keyword Insertion Deletion"; break; }
+      case "SVR": { this.longName = "String Value Replacement"; break; }
       case "TOR": { this.longName = "Transaction Origin Replacement"; break; }
+      case "TRC": { this.longName = "Transfer Return Check"; break; }
+      case "UBO": { this.longName = "Unchecked Block Optimization"; break; }
       case "UORD": { this.longName = "Unary Operator Replacement and Deletion"; break; }
       case "VUR": { this.longName = "Variable Unit Replacement"; break; }
-      case "VVR": { this.longName = "Variable Visibility Replacement"; break; }
+      case "VVO": { this.longName = "Variable Visibility Optimization"; break; }
       default:
         break;
     }
